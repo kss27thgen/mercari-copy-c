0a9c38a5d09ec8bdbd405b351a20deac36d505c3
Oishi Yuikyo <s2008511@gmail.com>
2018-12-16 20:39:22 +0900

    Add mypages#support view

--- app/controllers/mypages_controller.rb
+++ app/controllers/mypages_controller.rb
@@ -9,4 +9,10 @@ class MypagesController < ApplicationController
   def email_password
   end
 
+  def support
+  end
+
+  def point
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,8 @@ Rails.application.routes.draw do
   get 'mypages/deliver_address' => 'mypages#deliver_address'
   get 'mypages/identification' => 'mypages#identification'
   get 'mypages/email_password' => 'mypages#email_password'
-
+  get 'mypages/support' => 'mypages#support'
+  get 'mypages/point' => 'mypages#point'
 end
 
 
