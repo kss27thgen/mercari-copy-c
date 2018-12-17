a84447ab96ef1146c42dcde64fce4aa159a16a6f
tsukishima <hir01994o915@gmail.com>
2018-12-17 17:11:53 +0900

    デプロイのための変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'roots#index'
+  root 'toppages#index'
   get 'mypages/deliver_address' => 'mypages#deliver_address'
   get 'mypages/identification' => 'mypages#identification'
   get 'mypages/email_password' => 'mypages#email_password'
