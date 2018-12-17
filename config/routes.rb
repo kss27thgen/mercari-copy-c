9d25e01e7273d5ba32a2a59021d840ac51d9228f
hidenari-ueno <6chtm.ksda0@gmail.com>
2018-12-17 20:03:07 +0900

    Edit

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'root#index'
-  get 'root/mypage' => 'root#mypage'
+  root 'toppages#index'
+  get 'mypages/deliver_address' => 'mypages#deliver_address'
+  get 'mypages/identification' => 'mypages#identification'
+  get 'mypages/email_password' => 'mypages#email_password'
+  get 'mypages/notification' => 'mypages#notification'
+  get 'mypages/support' => 'mypages#support'
+  get 'mypages/point' => 'mypages#point'
+  get 'mypages/sales' => 'mypages#sales'
+  get 'help_center/index' => 'help_center＃index'
+  get 'items/new'
+
 end
