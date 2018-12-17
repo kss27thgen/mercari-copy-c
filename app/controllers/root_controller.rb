59d8f2188bbca30ea49d1bad9cdd3e29042a7cc5
hidenari-ueno <6chtm.ksda0@gmail.com>
2018-12-17 16:37:09 +0900

    done mypage

--- app/controllers/root_controller.rb
+++ app/controllers/root_controller.rb
@@ -3,5 +3,8 @@ class RootController < ApplicationController
   def index
   end
 
+
+  def mypage
+  end
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'root#index'
+  get 'root/mypage' => 'root#mypage'
 end
