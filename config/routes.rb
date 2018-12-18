a9a435c45aaa7391817d70249776414dcc689f83
RyuSeungChanne <ryuseungchanne@ryuushousannoMacBook-puro.local>
2018-12-18 14:50:28 +0900

    Revise

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-<<<<<<< HEAD
-  root 'root#index'
-  get '/root/mypage/card/' => 'root#mypage_card'
-=======
   root 'toppages#index'
   get 'mypages/deliver_address' => 'mypages#deliver_address'
   get 'mypages/identification' => 'mypages#identification'
@@ -15,5 +11,5 @@ Rails.application.routes.draw do
   get 'help_center/index' => 'help_center＃index'
   get 'items/new'
   get 'mypages/main' => 'mypages#main'
->>>>>>> be70a77ef12045bb9a392e68c77cbf91bbeb5f7a
+  get '/root/mypage/card/' => 'root#mypage_card'
 end
