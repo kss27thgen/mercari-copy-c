347ae9d15553d358575529b27a7b3ca3a7a0353e
hidenari-ueno <6chtm.ksda0@gmail.com>
2018-12-19 19:28:24 +0900

    Commit

---
+++ db/migrate/20181219095416_create_items.rb
@@ -0,0 +1,18 @@
+class CreateItems < ActiveRecord::Migration[5.1]
+  def change
+    create_table :items do |t|
+      t.string :name, null: false
+      t.text :explaination, null: false
+      t.integer :price, null: false
+      t.integer :status, null: false
+      t.string :shipping_fare, null: false
+      t.integer :shipping_region, null: false
+      t.string :shipping_shcedule, null: false
+      t.string :shipping_method, null: false
+      t.string :size
+      t.references :seller_id, foreign_key: true
+      t.references :buyer_id, foreign_key: true
+      t.timestamps
+    end
+  end
+end
