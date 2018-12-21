9b7eab6e912fd3a3c3d7be0c327407471e4ee1b7
RyuSeungChanne <ryuseungchanne@ryuushousannoMacBook-puro.local>
2018-12-21 13:31:09 +0900

    revise

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20181220102238) do
 
   create_table "item_categories", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
@@ -29,7 +28,6 @@ ActiveRecord::Schema.define(version: 20181220102238) do
     t.index ["item_id"], name: "index_item_images_on_item_id"
   end
 
-
   create_table "items", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "name", null: false
     t.text "explaination", null: false
@@ -44,7 +42,6 @@ ActiveRecord::Schema.define(version: 20181220102238) do
     t.datetime "updated_at", null: false
     t.integer "seller_id"
     t.integer "buyer_id"
-
   end
 
   create_table "sns_credentials", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
@@ -56,8 +53,6 @@ ActiveRecord::Schema.define(version: 20181220102238) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_sns_credentials_on_user_id"
-
-
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
@@ -74,8 +69,6 @@ ActiveRecord::Schema.define(version: 20181220102238) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-
-  add_foreign_key "sns_credentials", "users"
   add_foreign_key "item_images", "items"
-
+  add_foreign_key "sns_credentials", "users"
 end
