b2fc907ecd1589e9a8eed50bc48affdf946154c6
RyuSeungChanne <ryuseungchanne@ryuushousannoMacBook-puro.local>
2018-12-22 16:59:16 +0900

    Revise

--- config/routes.rb
+++ config/routes.rb
@@ -15,11 +15,7 @@ Rails.application.routes.draw do
   get 'mypages/support'
   get 'mypages/point'
   get 'mypages/sales'
-<<<<<<< HEAD
   get 'mypages/selling_item' => 'mypages#selling_item'
-=======
   get 'mypages/sign_up'
->>>>>>> d5ced14ebbd0a90553de330ca8d777d3699a8708
   get 'help_center/index' => 'help_center＃index'
-  root 'toppages#index'
 end
