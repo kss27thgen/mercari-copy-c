64b3e8fcbed5d0f3942a9e8bdc25f54a0348e62a
Oishi Yuikyo <s2008511@gmail.com>
2018-12-23 14:59:32 +0900

    layout fix

--- config/application.rb
+++ config/application.rb
@@ -8,13 +8,13 @@ Bundler.require(*Rails.groups)
 
 module MercariCopyC
   class Application < Rails::Application
-    config.to_prepare do
-      Devise::SessionsController.layout "simpleLayout"
-      Devise::RegistrationsController.layout "noLayout"
-      Devise::ConfirmationsController.layout "simpleLayout"
-      Devise::UnlocksController.layout "simpleLayout"
-      Devise::PasswordsController.layout "simpleLayout"
-    end
+    # config.to_prepare do
+    #   Devise::SessionsController.layout "simpleLayout"
+    #   Devise::RegistrationsController.layout "noLayout"
+    #   Devise::ConfirmationsController.layout "simpleLayout"
+    #   Devise::UnlocksController.layout "simpleLayout"
+    #   Devise::PasswordsController.layout "simpleLayout"
+    # end
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
