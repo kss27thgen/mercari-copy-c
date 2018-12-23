fe8fb2e43e831b536666a1e974483a467db3da79
Oishi Yuikyo <s2008511@gmail.com>
2018-12-23 14:58:25 +0900

    Add users::RegistrationsController

---
+++ app/controllers/users/registrations_controller.rb
@@ -0,0 +1,22 @@
+class Users::RegistrationsController < Devise::RegistrationsController
+  prepend_before_action :check_captcha, only: [:create]
+  prepend_before_action :configure_permitted_parameters, only: [:create]
+
+  def new
+    # render :layout => 'simpleLayout'
+    @user = User.new
+  end
+private
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
+  end
+
+  def check_captcha
+    self.resource = resource_class.new sign_up_params
+    resource.validate
+    set_minimum_password_length
+    unless verify_recaptcha(model: resource)
+      respond_with resource
+    end
+  end
+end
