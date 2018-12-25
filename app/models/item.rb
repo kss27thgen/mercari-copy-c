fbf91d1b089ca9b9271197fae644210ae4fec3d3
Oishi Yuikyo <s2008511@gmail.com>
2018-12-25 19:23:38 +0900

    Merge branch 'master' of https://github.com/hironori-tsukishima/mercari-copy-c into addMysellingItemspage

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,7 @@
 class Item < ApplicationRecord
   belongs_to :seller, class_name: "User", foreign_key: "seller_id"
   belongs_to :buyer, class_name: "User", foreign_key: "buyer_id"
-  has_many :item_images
+  has_many :item_images, dependent: :destroy
   accepts_nested_attributes_for :item_images
 end
+
