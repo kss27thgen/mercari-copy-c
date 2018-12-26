538fea5dc5628a3090ec33a3662f8f01d11cfd07
tsukishima <hir01994o915@gmail.com>
2018-12-26 14:20:41 +0900

    image→item_image

--- db/migrate/20181220102238_rename_item_images_column_to_image.rb
+++ db/migrate/20181220102238_rename_item_images_column_to_image.rb
@@ -1,5 +1,5 @@
 class RenameItemImagesColumnToImage < ActiveRecord::Migration[5.1]
   def change
-    rename_column :item_images, :item_images, :image
+    rename_column :item_images, :item_images, :item_images
   end
 end
