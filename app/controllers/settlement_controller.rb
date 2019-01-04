765cb012718828527f36f90e592469c12439da29
hidenari-ueno <6chtm.ksda0@gmail.com>
2019-01-04 18:39:05 +0900

    Edit

---
+++ app/controllers/settlement_controller.rb
@@ -0,0 +1,15 @@
+class SettlementController < ApplicationController
+  def create
+    @item = Item.find(params[:format])
+    @images = @item.item_images
+    @settlement = Item.find(params[:format])
+    @settlement.with_lock do
+      Payjp.api_key = ''
+      Payjp::Charge.create(
+        amount: @settlement.price,
+        card: params['payjp-token'],
+        currency: 'jpy'
+        )
+    end
+  end
+end
--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -8,9 +8,14 @@ class ToppagesController < ApplicationController
     @item = Item.find(1)
     @image = ItemImage.all
   end
+
+  def pay
+    payjp.api_key = 'sk_test_90414862343d84d073c615a7
+'
+    Payjp::Charge.create(
+    amount: 3500,
+    card: params['pay-token'],
+    currency: 'jpy'
+    )
+  end
 end
-
-
-
-
-
