95881d5a08dc94aeb93fbb7e562c34688ad658fb
hidenari-ueno <6chtm.ksda0@gmail.com>
2019-01-04 19:29:55 +0900

    Edit3

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -9,13 +9,4 @@ class ToppagesController < ApplicationController
     @image = ItemImage.all
   end
 
-  def pay
-    payjp.api_key = 'sk_test_90414862343d84d073c615a7
-'
-    Payjp::Charge.create(
-    amount: 3500,
-    card: params['pay-token'],
-    currency: 'jpy'
-    )
-  end
 end
