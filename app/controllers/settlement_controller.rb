a7f416ef0b07f21158ebbaea3cf68ae56a5d2ba1
hidenari-ueno <6chtm.ksda0@gmail.com>
2019-01-04 19:46:12 +0900

    Edit4

--- app/controllers/settlement_controller.rb
+++ app/controllers/settlement_controller.rb
@@ -4,7 +4,7 @@ class SettlementController < ApplicationController
     @images = @item.item_images
     @settlement = Item.find(params[:format])
     @settlement.with_lock do
-      Payjp.api_key = 'sk_test_90414862343d84d073c615a7'
+      Payjp.api_key = ENV['PAYJP_APP_SECRET']
       Payjp::Charge.create(
         amount: @settlement.price,
         card: params['payjp-token'],
