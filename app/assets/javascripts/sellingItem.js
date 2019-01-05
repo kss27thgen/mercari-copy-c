1047111b49d19b60351a40a838c2c5de69f83872
Oishi Yuikyo <s2008511@gmail.com>
2019-01-05 14:13:36 +0900

    削除機能のためのモーダルをJSからjQueryで書き換え

--- app/assets/javascripts/sellingItem.js
+++ app/assets/javascripts/sellingItem.js
@@ -1,29 +1,19 @@
 $(function(){
 // photoSlide
-  window.addEventListener("load", function() {
-    $(".wrapperSellingItemInfoLeftSubimages").mouseenter(function(){
-      var index = $(".wrapperSellingItemInfoLeftSubimages").index(this);
-      var slide = (-304 * index);
-      $(".wrapperSellingItemInfoLeftMainimage").animate( { left: slide + "px"},500 );
-      $(".wrapperSellingItemInfoLeftSubimages").css("opacity","0.5");
-      $(".wrapperSellingItemInfoLeftSubimages").eq(index).css("opacity","1")
-     });
+  $(".wrapperSellingItemInfoLeftSubimages").mouseenter(function(){
+    var index = $(".wrapperSellingItemInfoLeftSubimages").index(this);
+    var slide = (-304 * index);
+    $(".wrapperSellingItemInfoLeftMainimage").animate( { left: slide + "px"},500 );
+    $(".wrapperSellingItemInfoLeftSubimages").css("opacity","0.5");
+    $(".wrapperSellingItemInfoLeftSubimages").eq(index).css("opacity","1")
+   });
 
 // Modal
-    let open = document.getElementById('open');
-    let cancel = document.getElementById('cancel');
-    let modal = document.getElementById('modal');
-    let mask = document.getElementById('mask');
-    open.addEventListener('click', function(){
-      mask.className = '';
-      modal.className = '';
-    })
-    mask.addEventListener('click', function(){
-      modal.className = 'hidden';
-      mask.className = 'hidden';
-    })
-    cancel.addEventListener('click', function(){
-      mask.click();
-    })
-  }, false);
+  $('#open').on('click', function(){
+    $('#mask, #modal').removeClass('hidden');
+  });
+  $('#mask, #cancel').on('click', function(){
+    $('#mask, #modal').addClass('hidden');
+  });
+
 });
