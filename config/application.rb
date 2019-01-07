2071da686f7c28dfca3b5012c08297267be764ca
hidenari-ueno <6chtm.ksda0@gmail.com>
2019-01-07 21:00:26 +0900

    some view files

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,7 @@ module MercariCopyC
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
     config.i18n.default_locale = :ja
+    config.logger = Logger.new(STDOUT)
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
