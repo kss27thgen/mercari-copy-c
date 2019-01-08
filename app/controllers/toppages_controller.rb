c9408052a73980288728bc2436420f82762246e6
Oishi Yuikyo <s2008511@gmail.com>
2019-01-08 13:48:25 +0900

    カテゴリ別商品一覧のアクション作成

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -10,7 +10,48 @@ class ToppagesController < ApplicationController
     @anotheritems = Item.where(seller: @item.seller).where.not(id: @item)
   end
 
-  def category_index
+  def small_category_show
+  # params[:id]をScateのidとして取得
+    # 選ばれたScateの1つのインスタンス
+    @selected_Scate = SmallCategory.find(params[:id])
+    # 選ばれたScate全アイテムのインスタンス配列
+    @selected_cate_items = Item.where(small_category_id: params[:id])
   end
 
+  def middle_category_show
+  # params[:id]をMcateのidとして取得
+    # 選ばれたMcateの1つのインスタンス
+    @selected_Mcate = MiddleCategory.find(params[:id])
+    # 選ばれたMcateに紐づくScateのインスタンス配列
+    @related_Scates = SmallCategory.where(middle_category_id: params[:id])
+      # 上配列に紐づく(選ばれたMcateの全)アイテムのインスタンス配列(多次元配列)
+      @selected_cate_items = []
+      @related_Scates.each do |related_Scate|
+        @selected_cate_items << Item.where(small_category_id: related_Scate.id)
+      end
+      # 上配列を平坦化
+      @selected_cate_items.flatten!
+  end
+
+  def large_category_show
+  # params[:id]をLcateのidとして取得
+    # 選ばれたLcateの1つのインスタンス
+    @selected_Lcate = LargeCategory.find(params[:id])
+    # 選ばれたLcateに紐づくMcateのインスタンス配列
+    @related_Mcates = MiddleCategory.where(large_category_id: params[:id])
+      # 上配列に紐づくScateのインスタンス配列(多次元配列)
+      @related_Scates = []
+      @related_Mcates.each do |related_Mcate|
+        @related_Scates << SmallCategory.where(middle_category_id: related_Mcate.id)
+      end
+      # 上配列を平坦化
+      @related_Scates.flatten!
+        # 上配列に紐づく(選ばれたLcateの全)アイテムのインスタンス配列(多次元配列)
+        @selected_cate_items = []
+        @related_Scates.each do |related_Scate_L|
+          @selected_cate_items << Item.where(small_category_id: related_Scate_L.id)
+        end
+        # 上配列を平坦化
+        @selected_cate_items.flatten!
+  end
 end
