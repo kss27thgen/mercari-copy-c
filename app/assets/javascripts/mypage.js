64f9dcb5887d38378eeebb1967bcfb14ac5b9b04
hidenari-ueno <6chtm.ksda0@gmail.com>
2019-01-08 16:13:45 +0900

    retouched

--- app/assets/javascripts/mypage.js
+++ app/assets/javascripts/mypage.js
@@ -1,19 +1,23 @@
-$(function(){
-  $(".activeTop").on("click", function(){
-    var index =$(".activeTop").index(this);
-    $(".listBox p").css("display","none");
-    $(".listBox p").eq(index).css("display","block");
-    $(".clicked").removeClass('clicked').css("background-color","#eee");
-    $(this).addClass('clicked').css("background-color","white");
-  });
-});
+$(document).on('turbolinks:load', function(){
 
-$(function(){
-  $(".activeBottom").on("click", function(){
-    var index =$(".activeBottom").index(this);
-    $(".listBoxBottom p").css("display","none");
-    $(".listBoxBottom p").eq(index).css("display","block");
-    $(".selected").removeClass('selected').css("background-color","#eee");
-    $(this).addClass('selected').css("background-color","white");
+  $(function(){
+    $(".activeTop").on("click", function(){
+      var index =$(".activeTop").index(this);
+      $(".listBox p").css("display","none");
+      $(".listBox p").eq(index).css("display","block");
+      $(".clicked").removeClass('clicked').css("background-color","#eee");
+      $(this).addClass('clicked').css("background-color","white");
+    });
   });
+
+  $(function(){
+    $(".activeBottom").on("click", function(){
+      var index =$(".activeBottom").index(this);
+      $(".listBoxBottom p").css("display","none");
+      $(".listBoxBottom p").eq(index).css("display","block");
+      $(".selected").removeClass('selected').css("background-color","#eee");
+      $(this).addClass('selected').css("background-color","white");
+    });
+  });
+
 });
