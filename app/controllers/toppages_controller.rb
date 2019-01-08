a8a2bea89104f68ec4846cd441a5063d4a7f509c
Oishi Yuikyo <s2008511@gmail.com>
2019-01-08 15:36:22 +0900

    コメント削除

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -11,47 +11,32 @@ class ToppagesController < ApplicationController
   end
 
   def small_category_show
-  # params[:id]をScateのidとして取得
-    # 選ばれたScateの1つのインスタンス
     @selected_Scate = SmallCategory.find(params[:id])
-    # 選ばれたScate全アイテムのインスタンス配列
     @selected_Scate_items = @selected_Scate.items
   end
 
   def middle_category_show
-  # params[:id]をMcateのidとして取得
-    # 選ばれたMcateの1つのインスタンス
     @selected_Mcate = MiddleCategory.find(params[:id])
-    # 選ばれたMcateに紐づくScateのインスタンス配列
     @related_Scates = @selected_Mcate.small_categories
-      # 上配列に紐づく(選ばれたMcateの全)アイテムのインスタンス配列(多次元配列)
       @selected_Mcate_items = []
       @related_Scates.each do |related_Scate|
         @selected_Mcate_items << related_Scate.items
       end
-      # 上配列を平坦化
       @selected_Mcate_items.flatten!
   end
 
   def large_category_show
-  # params[:id]をLcateのidとして取得
-    # 選ばれたLcateの1つのインスタンス
     @selected_Lcate = LargeCategory.find(params[:id])
-    # 選ばれたLcateに紐づくMcateのインスタンス配列
     @related_Mcates = @selected_Lcate.middle_categories
-      # 上配列に紐づくScateのインスタンス配列(多次元配列)
       @related_Scates = []
       @related_Mcates.each do |related_Mcate|
         @related_Scates << related_Mcate.small_categories
       end
-      # 上配列を平坦化
       @related_Scates.flatten!
-        # 上配列に紐づく(選ばれたLcateの全)アイテムのインスタンス配列(多次元配列)
         @selected_Lcate_items = []
         @related_Scates.each do |related_Scate|
           @selected_Lcate_items << related_Scate.items
         end
-        # 上配列を平坦化
         @selected_Lcate_items.flatten!
   end
 end
