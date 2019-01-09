e21b5a24559dc7eaa315504d1f28ada29aa93744
tsukishima <hir01994o915@gmail.com>
2019-01-09 12:23:28 +0900

    loggerの追加

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -11,6 +11,10 @@ Rails.application.configure do
   # Show full error reports.
   config.consider_all_requests_local = true
 
+  logger           = ActiveSupport::Logger.new(STDOUT)
+  logger.formatter = config.log_formatter
+  config.logger = ActiveSupport::TaggedLogging.new(logger)
+
   # Enable/disable caching. By default caching is disabled.
   if Rails.root.join('tmp/caching-dev.txt').exist?
     config.action_controller.perform_caching = true
