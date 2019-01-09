1d95a251dec7068d4d2bc1f525db8d35da80247a
Oishi Yuikyo <s2008511@gmail.com>
2019-01-09 13:40:14 +0900

    不要なアクションの記述削除

--- app/controllers/mypages_controller.rb
+++ app/controllers/mypages_controller.rb
@@ -1,39 +1,5 @@
 class MypagesController < ApplicationController
 
-  def deliver_address
-  end
-
-  def identification
-  end
-
-  def email_password
-  end
-
-
-  def notification
-  end
-
-  def support
-  end
-
-  def point
-  end
-
-  def sales
-  end
-
-  def main
-  end
-
-  def logout
-  end
-
-  def card
-  end
-
-  def info
-  end
-
   def sellingItem
     @item = Item.find(params[:id])
     @images = @item.item_images
@@ -59,10 +25,5 @@ class MypagesController < ApplicationController
     @items = Item.where(seller_id: current_user.id)
   end
 
-  def exhibitionItemTransaction
-  end
-
-  def exhibitionItemSold
-  end
 
 end
